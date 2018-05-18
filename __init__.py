ff4e9e4584451dec82cda3a7156a7e8361653a5e
rzeeman711 <joshua.k.rzemien@gmail.com>
2018-05-18 14:18:11 -0500

    fixed logging

--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
+import logging
+
 import config
 import basic
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s -> %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger(__name__)
 
 basic.bot.run(config.cfg["discord"]["token"])
